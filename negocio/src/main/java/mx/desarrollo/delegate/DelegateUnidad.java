98c1dfaa568479438657481f609acd05a3d8dc6b
vicentexchen <vicente.chen@uabc.edu.mx>
2026-09-22 14:49:16 -0700

    Se concluyo el PBI-UNI-Consultas y Modificaciones

--- negocio/src/main/java/mx/desarrollo/delegate/DelegateUnidad.java
+++ negocio/src/main/java/mx/desarrollo/delegate/DelegateUnidad.java
@@ -4,6 +4,7 @@ import mx.desarrollo.entity.Unidadaprendizaje;
 import mx.desarrollo.persistence.integration.ServiceLocator;
 
 import java.util.List;
+import java.util.Optional;
 
 public class DelegateUnidad {
     public void saveUnidad(Unidadaprendizaje unidad) {
@@ -12,5 +13,16 @@ public class DelegateUnidad {
 
     public List<Unidadaprendizaje> obtenerTodos() {
         return ServiceLocator.getInstanceUnidadDAO().findAll();
+    }
+
+    public List<Unidadaprendizaje> buscarporNombre(String nombre){
+        return ServiceLocator.getInstanceUnidadDAO().findByOneParameter(nombre,"nombreUnidad");
+    }
+    public Unidadaprendizaje obtenerPorId(Integer id){
+        Optional<Unidadaprendizaje> encontrada = ServiceLocator.getInstanceUnidadDAO().find(id);
+        return encontrada.orElse(null);
+    }
+    public void modificarUnidad(Unidadaprendizaje unidad){
+        ServiceLocator.getInstanceUnidadDAO().update(unidad);
     }
 }
